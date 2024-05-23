04f554eba27cd62376a6909e70197bfd6412927c
ItsSitanshu <sitanshu15shrestha@gmail.com>
2024-05-23 16:32:22 +0545

    After fixing issue highlighted in: 05ba55c, Remove testing code

--- src/main.c
+++ src/main.c
@@ -10,13 +10,6 @@ int main(int argc, char* argv[]) {
     Parser* parser = parser_init((char*)argv[1]);
 
     parser_parse(parser);
-
-    // Token* token = lexer_next_token(parser->lexer);
-
-    // while (token->type != TOK_EOF) {
-    //     token = lexer_next_token(parser->lexer);
-    // }
-
     parser_free(parser);
 
     return 0;
